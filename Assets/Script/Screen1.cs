b80595e114c4470d4f90722ce4a532fe2cace4a9
Biju Borah <biju20_ug@ee.nits.ac.in>
2021-10-03 11:42:40 +0530

    v1.0

---
+++ Assets/Script/Screen1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screen1 : MonoBehaviour
+{
+    public GameObject Start_screen;
+    public GameObject Ready_button;
+    public GameObject Play;
+    public screen2 s2;
+    int num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Start_screen.SetActive(true);
+        Ready_button.SetActive(true);
+        Play.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Ready_pressed()
+    {
+        Ready_button.SetActive(false);
+        Play.SetActive(true);
+    }
+
+    public void scissor()
+    {
+        num = 1;
+        Start_screen.SetActive(false);
+        s2.AI(num);
+    }
+    public void paper()
+    {
+        num = 2;
+        Start_screen.SetActive(false);
+        s2.AI(num);
+    }
+    public void stone()
+    {
+        num = 3;
+        Start_screen.SetActive(false);
+        s2.AI(num);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Script/screen2.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen2 : MonoBehaviour
+{
+    public GameObject screen_1;
+    public GameObject screen_2;
+    public GameObject screen_3;
+    public GameObject screen_4;
+    float ran_num;
+    float score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screen_2.SetActive(false);
+        screen_3.SetActive(false);
+        screen_4.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AI(int num)
+    {
+
+        ran_num = Random.Range(1,4);
+        if(num == 1){
+            if(ran_num ==1){
+                score +=0.5f;
+                screen_4.SetActive(true);
+            }
+            else if(ran_num == 2){
+                score +=1.0f;
+                screen_2.SetActive(true);
+            }
+            else{
+                score +=0.0f;
+                screen_3.SetActive(true);
+            }
+            
+        }
+        else if(num ==2){
+            if(ran_num ==2){
+                score +=0.5f;
+                screen_4.SetActive(true);
+            }
+            else if(ran_num == 1){
+                score +=0.0f;
+                screen_3.SetActive(true);
+            }
+            else{
+                score +=1.0f;
+                screen_2.SetActive(true);
+            }
+        }
+        else{
+            if(ran_num ==3){
+                score +=0.5f;
+                screen_4.SetActive(true);
+            }
+            else if(ran_num == 1){
+                score +=0.0f;
+                screen_3.SetActive(true);
+            }
+            else{
+                score +=1.0f;
+                screen_2.SetActive(true);
+            }
+        }
+    }
+    public void re_screen()
+    {
+        screen_2.SetActive(false);
+        screen_3.SetActive(false);
+        screen_4.SetActive(false);
+        screen_1.SetActive(true);
+    }
+}
